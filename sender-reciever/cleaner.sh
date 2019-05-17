f30a7149276da555f4d6c0c4a53b188553ed419b
EliasChahine <eliascha97@gmail.com>
2019-05-17 13:30:14 +0200

    Made code for filtering field test data, added mathematica code for report.

--- sender-reciever/cleaner.sh
+++ sender-reciever/cleaner.sh
@@ -7,5 +7,7 @@ sudo rm /home/pi/sender-reciever/lora/recCoordinates
 sudo rm /home/pi/sender-reciever/lora/__init__.pyc
 sudo rm /home/pi/sender-reciever/lora/dragino_lora_app
 sudo rm /home/pi/sender-reciever/lora/rpi-transceiver-main.o
+sudo rm /home/pi/testingDayTwo/counterValue
+sudo rm /home/pi/testingDayTwo/recCounterValues
 cd /home/pi/sender-reciever/lora/
 make
